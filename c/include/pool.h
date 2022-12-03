a0e204d7ab7a560390e78207ea3426baad2b3a52
Bruno Raoult <braoult@gmail.com>
2022-12-03 16:10:37 +0100

    update (C) date

--- c/include/pool.h
+++ c/include/pool.h
@@ -1,6 +1,6 @@
 /* pool.h - A simple memory pool manager.
  *
- * Copyright (C) 2021 Bruno Raoult ("br")
+ * Copyright (C) 2021-2022 Bruno Raoult ("br")
  * Licensed under the GNU General Public License v3.0 or later.
  * Some rights reserved. See COPYING.
  *
